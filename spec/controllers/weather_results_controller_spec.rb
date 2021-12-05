42541327f482f11a3a748dd60b3b4d82b741afc6
Freddie Rigby <rigbyfreddie685@gmail.com>
2021-12-05 11:04:54 +0000

    weather result controller testing, visiting show with no weather result redirects to root

--- spec/controllers/weather_results_controller_spec.rb
+++ spec/controllers/weather_results_controller_spec.rb
@@ -5,6 +5,11 @@ require 'rails_helper'
 RSpec.describe WeatherResultsController, type: :controller do
   include_context 'stubs mixed validity requests'
 
+  it 'show redirects to root if theres no weather_result' do
+    get :show
+    expect(response).to redirect_to(root_url)
+  end
+
   it 'renders show from a valid UK postcode' do
     post :create, params: { postcode: 'sw12ny' }
     expect(response).to render_template(:show)
