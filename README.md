12f6d6c220d33c72a8943b6a851842a61f14d2ff
Freddie Rigby <rigbyfreddie685@gmail.com>
2021-12-05 12:19:53 +0000

    Updated rubocop config to exclude ruby generated files from webpacker reinstall

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ bundle install/update
 
 Set an environment variable weather_api_key which is your key for accessing weatherapi
 
+rails db:create
+
+rails db:migrate and rails db:migrate RAILS_ENV=test
+
+It may be you need to reinstall webpacker with rails webpacker:install
+
 rspec runs the tests
 
 rubocop runs the linting
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_201_161_931) do
-  create_table 'temperature_thresholds', force: :cascade do |t|
-    t.float 'warm_temperature'
-    t.float 'hot_temperature'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 2021_12_01_161931) do
+
+  create_table "temperature_thresholds", force: :cascade do |t|
+    t.float "warm_temperature"
+    t.float "hot_temperature"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
